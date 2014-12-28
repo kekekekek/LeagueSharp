d9856bcf5ad9135c6f97baf5a880e27470b0201e
Krzysztof Czelusniak <krzysztof@czelusniak.com>
2014-12-28 15:54:10 -0600

    keLux: Added champion check
    Added champion check to ensure Lux logic is only used with Lux

--- keLux/Program.cs
+++ keLux/Program.cs
@@ -1,17 +1,42 @@
-﻿using System;
+﻿#region LICENSE
+
+// This file is part of keLux.
+// Copyright (C) 2014  kekek
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with LeagueSharp.Common.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System;
+using System.Diagnostics.CodeAnalysis;
 using LeagueSharp.Common;
+
+#endregion
 
 namespace keLux
 {
     internal class Program
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "arguments")]
+        [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "arguments")]
         private static void Main(string[] arguments)
         {
             CustomEvents.Game.OnGameLoad += OnGameLoad;
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", MessageId = "keLux.Lux")]
+        [SuppressMessage("Microsoft.Usage", "CA1806:DoNotIgnoreMethodResults", MessageId = "keLux.Lux")]
         private static void OnGameLoad(EventArgs arguments)
         {
             new Lux();
--- keLux/Properties/AssemblyInfo.cs
+++ keLux/Properties/AssemblyInfo.cs
@@ -1,25 +1,52 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿#region LICENSE
+
+// This file is part of keLux.
+// Copyright (C) 2014  kekek
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with LeagueSharp.Common.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("keLux")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("keLux")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("kekek")]
 [assembly: AssemblyProduct("keLux")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright (C) 2014  kekek")]
+[assembly: AssemblyTrademark("kekek")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("15818187-9153-40d6-9b57-580dc83f632c")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +59,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
