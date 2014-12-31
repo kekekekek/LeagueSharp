1e7ab40f065b7d2c0167a05a092872519fff5c88
Krzysztof Czelusniak <krzysztof@czelusniak.com>
2014-12-30 22:54:49 -0600

    Added keMinimap
    Added keMinimap to allow developers to access and edit Minimap
    properties

---
+++ keMinimap/GlobalSuppressions.cs
@@ -0,0 +1,48 @@
+#region LICENSE
+
+// This file is part of keMinimap.
+// Copyright (C) 2014  kekek
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System.Diagnostics.CodeAnalysis;
+
+#endregion
+
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Ke",
+        Scope = "namespace", Target = "KeMinimap")]
+[assembly: SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Minimap",
+        Scope = "namespace", Target = "KeMinimap")]
+[assembly: SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "ke")]
+[assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Minimap")]
+[assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "ke")]
+[assembly:
+    SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Ke",
+        Scope = "namespace", Target = "KeMinimap")]
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+//
+// To add a suppression to this file, right-click the message in the 
+// Code Analysis results, point to "Suppress Message", and click 
+// "In Suppression File".
+// You do not need to add suppressions to this file manually.
---
+++ keMinimap/Minimap.cs
@@ -0,0 +1,87 @@
+#region LICENSE
+
+// This file is part of keMinimap.
+// Copyright (C) 2014  kekek
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: CLSCompliant(true)]
+
+namespace KeMinimap
+{
+    [SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Minimap")]
+    public static class Minimap
+    {
+        public static float Left
+        {
+            get { return Read(Offsets.Left); }
+            set { Write(Offsets.Left, value); }
+        }
+
+        public static float Top
+        {
+            get { return Read(Offsets.Top); }
+            set { Write(Offsets.Top, value); }
+        }
+
+        public static float Width
+        {
+            get { return Read(Offsets.Width); }
+            set { Write(Offsets.Width, value); }
+        }
+
+        public static float Height
+        {
+            get { return Read(Offsets.Height); }
+            set { Write(Offsets.Height, value); }
+        }
+
+        private static IntPtr InitializeBaseAddress()
+        {
+            return Marshal.ReadIntPtr(
+                Marshal.ReadIntPtr(Process.GetCurrentProcess().MainModule.BaseAddress + 0x14C901C), 0xD4);
+        }
+
+        private static float Read(Offsets property)
+        {
+            return (float)Marshal.PtrToStructure(BaseAddress + (int)property, typeof(float));
+        }
+
+        private static void Write(Offsets property, float value)
+        {
+            Marshal.StructureToPtr(value, BaseAddress + (int)property, false);
+        }
+
+        private enum Offsets
+        {
+            Left = 0xAC,
+            Top = 0xB0,
+            Width = 0xB4,
+            Height = 0xB8
+        }
+
+        private static readonly IntPtr BaseAddress = InitializeBaseAddress();
+    }
+}
