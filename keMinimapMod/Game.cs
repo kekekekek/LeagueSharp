1f306f2b25a4910313131e6a7bd5ae607521025a
Krzysztof Czelusniak <krzysztof@czelusniak.com>
2015-01-01 15:03:21 -0600

    Added keMinimapMod to edit position of minimap

---
+++ keMinimapMod/Game.cs
@@ -0,0 +1,54 @@
+﻿#region LICENSE
+
+// This file is part of keMinimapMod.
+// Copyright (C) 2014  kekek
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System;
+using System.Diagnostics;
+using LeagueSharp.Common;
+
+#endregion
+
+// ReSharper disable ConvertToExpressionBodyWhenPossible
+
+namespace keMinimapMod
+{
+    internal static class Game
+    {
+        private static MinimapMod _mod;
+        // ReSharper disable once UnusedParameter.Local
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += OnGameLoad;
+        }
+
+        private static void OnGameLoad(EventArgs arguments)
+        {
+            _mod = new MinimapMod();
+            LeagueSharp.Game.OnGameUpdate += OnGameUpdate;
+        }
+
+        private static void OnGameUpdate(EventArgs arguments)
+        {
+            Debug.Assert(_mod != null, "mod != null");
+            _mod.Update();
+        }
+    }
+}
---
+++ keMinimapMod/MinimapMod.cs
@@ -0,0 +1,100 @@
+﻿#region LICENSE
+
+// This file is part of keMinimapMod.
+// Copyright (C) 2014  kekek
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+#region
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using KeMinimap;
+using LeagueSharp;
+using LeagueSharp.Common;
+
+#endregion
+
+namespace keMinimapMod
+{
+    internal sealed class MinimapMod
+    {
+        private readonly Dictionary<Dimension, int> maxValues = new Dictionary<Dimension, int>
+        {
+            { Dimension.Horizontal, Drawing.Width },
+            { Dimension.Vertical, Drawing.Height }
+        };
+
+        private readonly MenuWrapper menu = new MenuWrapper("keMinimapMod", false, false);
+        private readonly Dictionary<string, MenuWrapper.SliderLink> values;
+        private int height;
+        private int left;
+        private int top;
+        private int width;
+
+        internal MinimapMod()
+        {
+            values = new Dictionary<string, MenuWrapper.SliderLink>
+            {
+                { "left", Slider("Left", Minimap.Left, Dimension.Horizontal) },
+                { "top", Slider("Top", Minimap.Top, Dimension.Vertical) },
+                { "width", Slider("Width", Minimap.Width, Dimension.Horizontal) },
+                { "height", Slider("Height", Minimap.Height, Dimension.Vertical) }
+            };
+            UpdateValues();
+        }
+
+        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        internal void Update()
+        {
+            Debug.Assert(values != null, "values != null");
+            if (left != values["left"].Value.Value || top != values["top"].Value.Value ||
+                width != values["width"].Value.Value || height != values["height"].Value.Value)
+            {
+                UpdateValues();
+            }
+        }
+
+        private MenuWrapper.SliderLink Slider(string property, float value, Dimension dimension)
+        {
+            Debug.Assert(menu != null, "menu != null");
+            Debug.Assert(menu.MainMenu != null, "menu.MainMenu != null");
+            Debug.Assert(maxValues != null, "maxValues != null");
+            return menu.MainMenu.AddLinkedSlider(property, Convert.ToInt32(value), 0, maxValues[dimension]);
+        }
+
+        private enum Dimension
+        {
+            Horizontal,
+            Vertical
+        }
+
+        private void UpdateValues()
+        {
+            Debug.Assert(values != null, "values != null");
+            // ReSharper disable once PossibleNullReferenceException
+            Minimap.Left = left = values["left"].Value.Value;
+            // ReSharper disable once PossibleNullReferenceException
+            Minimap.Top = top = values["top"].Value.Value;
+            // ReSharper disable once PossibleNullReferenceException
+            Minimap.Width = width = values["width"].Value.Value;
+            // ReSharper disable once PossibleNullReferenceException
+            Minimap.Height = height = values["height"].Value.Value;
+        }
+    }
+}
